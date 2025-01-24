a3e217fe362c261f8c8f453dcf65c73072ad9999
kalabarigi <82603974+kalabarigi@users.noreply.github.com>
2025-01-24 13:48:01 +0000

    Update c3-vpc.tf

--- c3-vpc.tf
+++ c3-vpc.tf
@@ -1,12 +1,12 @@
 # Resource: VPC
-resource "google_compute_network" "myvpc" {
-  name = "vpc1"
+resource "google_compute_network" "tcsvpc" {
+  name = "tcsvpc"
   auto_create_subnetworks = false   
 }
 
 # Resource: Subnet
-resource "google_compute_subnetwork" "mysubnet" {
-  name = "subnet1"
+resource "google_compute_subnetwork" "vmmysubnet" {
+  name = "vmsubnet1"
   region = "europe-west2"
   ip_cidr_range = "10.128.0.0/20"
   network = google_compute_network.myvpc.id 
